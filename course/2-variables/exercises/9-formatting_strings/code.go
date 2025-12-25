f74a0f2bd743acabf5c2fef9b334c8064aeb3a58
dannythedataanalyst911 <iamphuocvux@gmail.com>
2025-12-25 14:44:05 +0200

    modified:   course/2-variables/exercises/9-formatting_strings/code.go

--- course/2-variables/exercises/9-formatting_strings/code.go
+++ course/2-variables/exercises/9-formatting_strings/code.go
@@ -6,7 +6,7 @@ func main() {
 	const name = "Saul Goodman"
 	const openRate = 30.5
 
-	// ?
+	msg := fmt.Sprintf("Hi %s! Your open rate is %.1f%%.", name, openRate)
 
 	// don't edit below this line
 
